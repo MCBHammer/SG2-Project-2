eeaa162cf710838d9fa204fa80ce5471817b260a
MCBHammer <armanaznavour@yahoo.com>
2021-10-18 17:19:59 -0500

    Editor Tutorial Part 3

---
+++ Assets/Scripts/Types.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Types
+{
+    public enum MageDmgType
+    {
+        FIRE,
+        ICE
+    }
+    public enum MageWpnType
+    {
+        STAFF,
+        WAND
+    }
+    public enum WarriorClassType
+    {
+        DEFENDER,
+        BERSERKER
+    }
+    public enum WarriorWpnType
+    {
+        ONE_HANDED_SWORD,
+        TWO_HANDED_SWORD,
+        DUAL_WIELDED_SWORDS
+    }
+    public enum RogueStrategyType
+    {
+        DAGGERS,
+        BOW
+    }
+    public enum RogueWpnType
+    {
+        STEALTH,
+        SPEED
+    }
+}
