0adbb26c1e9bfaef029b72b64000f5abae907f1a
MCBHammer <armanaznavour@yahoo.com>
2021-10-18 22:31:11 -0500

    Editor Tutorial Part 4

--- Assets/Editor/EnemyDesignerWindow.cs
+++ Assets/Editor/EnemyDesignerWindow.cs
@@ -5,6 +5,18 @@ using UnityEditor;
 
 public class EnemyDesignerWindow : EditorWindow
 {
+    Texture2D headerSectionTexture;
+    Texture2D mageSectionTexture;
+    Texture2D warriorSectionTexture;
+    Texture2D rogueSectionTexture;
+
+    Color headerSectionColor = new Color(13f / 255f, 32f / 255f, 44f / 255f, 1f);
+
+    Rect headerSection;
+    Rect mageSection;
+    Rect warriorSection;
+    Rect rogueSection;
+
     [MenuItem("Window/Enemy Designer")]
     static void OpenWindow()
     {
@@ -12,5 +24,103 @@ public class EnemyDesignerWindow : EditorWindow
         window.minSize = new Vector2(600, 300);
         //window.maxSize
         window.Show();
+    }
+
+    void OnEnable()
+    {
+        InitTextures();
+    }
+
+    void InitTextures()
+    {
+        headerSectionTexture = new Texture2D(1, 1);
+        headerSectionTexture.SetPixel(0, 0, headerSectionColor);
+        headerSectionTexture.Apply();
+
+        //maybe use textures in resources
+        mageSectionTexture = new Texture2D(1, 1);
+        mageSectionTexture.SetPixel(0, 0, new Color(0f, 0f, 1f, 1f));
+        mageSectionTexture.Apply();
+
+        warriorSectionTexture = new Texture2D(1, 1);
+        warriorSectionTexture.SetPixel(0, 0, new Color(1f, 0f, 0f, 1f));
+        warriorSectionTexture.Apply();
+
+        rogueSectionTexture = new Texture2D(1, 1);
+        rogueSectionTexture.SetPixel(0, 0, new Color(0f, 1f, 0f, 1f));
+        rogueSectionTexture.Apply();
+    }
+
+    void OnGUI()
+    {
+        DrawLayouts();
+        DrawHeader();
+        DrawMageSettings();
+        DrawWarriorSettings();
+        DrawRogueSettings();
+    }
+
+    void DrawLayouts()
+    {
+        headerSection.x = 0;
+        headerSection.y = 0;
+        headerSection.width = Screen.width;
+        headerSection.height = 50;
+
+        mageSection.x = 0;
+        mageSection.y = headerSection.height;
+        mageSection.width = (Screen.width/3f);
+        mageSection.height = Screen.height - headerSection.height;
+
+        warriorSection.x = (Screen.width / 3f);
+        warriorSection.y = headerSection.height;
+        warriorSection.width = (Screen.width / 3f);
+        warriorSection.height = Screen.height - headerSection.height;
+
+        rogueSection.x = (Screen.width / 3f) * 2;
+        rogueSection.y = headerSection.height;
+        rogueSection.width = (Screen.width / 3f);
+        rogueSection.height = Screen.height - headerSection.height;
+
+        GUI.DrawTexture(headerSection, headerSectionTexture);
+        GUI.DrawTexture(mageSection, mageSectionTexture);
+        GUI.DrawTexture(warriorSection, warriorSectionTexture);
+        GUI.DrawTexture(rogueSection, rogueSectionTexture);
+    }
+
+    void DrawHeader()
+    {
+        GUILayout.BeginArea(headerSection);
+
+
+
+        GUILayout.EndArea();
+    }
+
+    void DrawMageSettings()
+    {
+        GUILayout.BeginArea(mageSection);
+
+
+
+        GUILayout.EndArea();
+    }
+
+    void DrawWarriorSettings()
+    {
+        GUILayout.BeginArea(warriorSection);
+
+
+
+        GUILayout.EndArea();
+    }
+
+    void DrawRogueSettings()
+    {
+        GUILayout.BeginArea(rogueSection);
+
+
+
+        GUILayout.EndArea();
     }
 }
